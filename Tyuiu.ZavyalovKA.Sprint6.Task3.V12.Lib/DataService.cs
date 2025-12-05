5066d4075f6e90d62b2ee70afaee5c51fa5155d1
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-12-05 14:24:17 +0500

    Task3

---
+++ Tyuiu.ZavyalovKA.Sprint6.Task3.V12.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.ZavyalovKA.Sprint6.Task3.V12.Lib
+{
+    public class DataService : ISprint6Task3V12
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+            for (int j = 0; j < columns; j++) 
+            {
+                if (matrix[0, j] % 2 == 0)
+                {
+                    matrix[0, j] = 0;
+                }
+            }
+            return matrix;
+        }
+    } 
+}
+
---
+++ Tyuiu.ZavyalovKA.Sprint6.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.ZavyalovKA.Sprint6.Task3.V12.Lib;
+namespace Tyuiu.ZavyalovKA.Sprint6.Task3.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = {
+                {-6, -13, -1,  -7,  10},
+                {14, -18, 18, 1, 11},
+                {-2, -17, -15, -10, -8},
+                {19, -4, -6, -11, 8},
+                {-17, 17,  14, 13, 19}
+            };
+            int[,] res = ds.Calculate(matrix);
+            int[,] wait = {
+                {0, -13, -1,  -7,  0},
+                {14, -18, 18, 1, 11},
+                {-2, -17, -15, -10, -8},
+                {19, -4, -6, -11, 8},
+                {-17, 17,  14, 13, 19}
+            };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
