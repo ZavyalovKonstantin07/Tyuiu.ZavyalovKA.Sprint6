7a2af980ecb4298f54c7e77e182cfc962c1cb5f6
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-12-06 14:55:28 +0500

    Task5

---
+++ Tyuiu.ZavyalovKA.Sprint6.Task5.V16.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.ZavyalovKA.Sprint6.Task5.V16.Lib;
+namespace Tyuiu.ZavyalovKA.Sprint6.Task5.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
