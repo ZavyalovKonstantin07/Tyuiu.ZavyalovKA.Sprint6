119ad11ff71599ca9bdbcb5e689f0ab39cd4b7a2
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-12-06 16:08:40 +0500

    Task6

---
+++ Tyuiu.ZavyalovKA.Sprint6.Task6.V20.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.ZavyalovKA.Sprint6.Task6.V20.Lib
+{
+    public class DataService : ISprint6Task6V20
+    {
+        public string CollectTextFromFile(string path)
+        {
+            StringBuilder resultBuilder = new StringBuilder();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (words.Length >= 2)
+                    {
+                        if (resultBuilder.Length > 0)
+                        {
+                            resultBuilder.AppendLine(); 
+                        }
+                        resultBuilder.Append(words[1]);
+                    }
+                }
+            }
+            return resultBuilder.ToString();
+        }
+    }
+}
---
+++ Tyuiu.ZavyalovKA.Sprint6.Task6.V20.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.ZavyalovKA.Sprint6.Task6.V20.Lib;
+namespace Tyuiu.ZavyalovKA.Sprint6.Task6.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
