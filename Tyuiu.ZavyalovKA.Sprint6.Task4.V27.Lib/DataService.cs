6a6ac25a6d54bedc4cf1ab75c1efe730fd213276
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-12-06 14:23:26 +0500

    Task4

--- Tyuiu.ZavyalovKA.Sprint6.Task4.V27.Lib/DataService.cs
+++ Tyuiu.ZavyalovKA.Sprint6.Task4.V27.Lib/DataService.cs
@@ -5,13 +5,21 @@ namespace Tyuiu.ZavyalovKA.Sprint6.Task4.V27.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            int length = (stopValue - startValue) + 1; 
+            int length = (stopValue - startValue) + 1;
             double[] valueArray = new double[length]; 
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                double y = (Math.Cos(x) / (x + 1)) - (Math.Cos(x) * 1.3) + (3 * x);
-                y = Math.Round(y, 2); 
+                double y;
+                if (x + 1 == 0)
+                {
+                    y = 0;
+                }
+                else
+                {
+                    y = (Math.Cos(x) / (x + 1)) - (Math.Cos(x) * 1.3) + (3 * x);
+                    y = Math.Round(y, 2); 
+                }
                 valueArray[count] = y; 
                 count++;
             }
